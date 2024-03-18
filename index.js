62fa172cee257843a23b2d8fc83e52ce5ca4f362
siyajari samuel <siyajari.samuel@student.moringaschool.com>
2024-03-18 14:28:14 +0300

    created JavaScript Variables

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+function companyName(){
+    const companyName = 'Scuber';
+}
+let mostProfitableNeighborhood = 'Chelsea';
+function mostProfitableNeighborhood(){
+    const mostProfitableNeighborhood = 'Chelsea';
+}
+let companyCeo = 'Susan Smith';
+function companyCeo(){
+    const companyCeo = ' Susan Smith';
+}
+
+
+
